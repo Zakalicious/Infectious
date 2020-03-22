4bf30aeca6af18f44495536383830934ecfbb19e
Thomas Hartmann <thomas.zwei.null@gmail.com>
2020-03-22 16:09:59 -0600

    SplitView (spatial SIR simulation | Chart)
    Chart is not synced to simulation, yet.

---
+++ Infectious/MainSplitViewController.swift
@@ -0,0 +1,21 @@
+//
+//  MainSplitViewController.swift
+//  Infectious
+//
+//  Created by Thomas Hartmann on 3/22/20.
+//  Copyright © 2020 Thomas Hartmann. All rights reserved.
+//
+
+import AppKit
+
+class MainSplitVC: NSSplitViewController {
+
+  //Outlet to the menu pane
+  //@IBOutlet weak var menuPane: NSSplitViewItem!
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+
+  }
+}
--- Infectious/ViewController.swift
+++ Infectious/ViewController.swift
@@ -6,22 +6,61 @@
 //  Copyright © 2020 Thomas Hartmann. All rights reserved.
 //
 
-import Cocoa
+import AppKit
 import SpriteKit
 import GameplayKit
+import Charts
 
 class ViewController: NSViewController {
 
     @IBOutlet var skView: SKView!
-    
+
+ /*   override func viewWillLayout() {
+        super.viewWillLayout()
+
+        if (skView == nil) {
+            let backgroundImageView = lineChartView
+            view.addSubview(backgroundImageView!)
+
+            skView = SKView(frame: CGRect(origin: CGPoint(), size: CGSize(width: 1536, height: 2048)))
+            //skView!.ignoreSiblingOrder = true
+            if let scene = SKScene(fileNamed: "GameScene") {
+                            // Set the scale mode to scale to fit the window
+                            scene.scaleMode = .aspectFit
+                            print(view.bounds, scene.frame)
+                            //view.setFrameSize(scene.frame)
+                            view.setNeedsDisplay(scene.frame)
+                            print(scene.anchorPoint)
+                            scene.anchorPoint = CGPoint(x:0,y:0)
+                            print(scene.anchorPoint)
+                            
+                            scene.physicsBody = SKPhysicsBody(edgeLoopFrom: scene.frame)
+            //scene.physicsBody?..
+                            
+                            // Present the scene
+                            skView.presentScene(scene)
+                        }
+        }
+    }
+*/
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        
         if let view = self.skView {
             // Load the SKScene from 'GameScene.sks'
             if let scene = SKScene(fileNamed: "GameScene") {
                 // Set the scale mode to scale to fit the window
-                scene.scaleMode = .aspectFill
+                scene.scaleMode = .aspectFit
+                print(view.bounds, scene.frame)
+                //view.setFrameSize(scene.frame)
+                view.setNeedsDisplay(scene.frame)
+                print(scene.anchorPoint)
+                scene.anchorPoint = CGPoint(x:0,y:0)
+                print(scene.anchorPoint)
+                
+                scene.physicsBody = SKPhysicsBody(edgeLoopFrom: scene.frame)
+//scene.physicsBody?..
                 
                 // Present the scene
                 view.presentScene(scene)
@@ -33,5 +72,6 @@ class ViewController: NSViewController {
             view.showsNodeCount = true
         }
     }
+
 }
 
